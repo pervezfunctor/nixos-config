d71029a7b0885b9ba7467f91804a411d9d82fd6e
Pervez Iqbal <pervefunctor@gmail.com>
2025-07-01 17:12:33 +0530

    fix flake?

--- flake.nix
+++ flake.nix
@@ -1,25 +1,18 @@
-# flake.nix
 {
   description = "NixOS Backup Box";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      in {
-        nixosConfigurations.backup-box = pkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/backup-box/configuration.nix
-          ];
-        };
-      });
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      backup-box = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/backup-box/configuration.nix
+        ];
+      };
+    };
+  };
 }
